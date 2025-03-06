6691bf1247154a17dcb60d5f534b1b897940681b
Emanuel1273 <willzekoficial@gmail.com>
2025-03-06 16:02:54 -0600

    Actualizar tools-meta_ai.js

--- plugins/tools-meta_ai.js
+++ plugins/tools-meta_ai.js
@@ -1,24 +1,26 @@
-// Código Creado Por WillZek Wa.me/50557865603
-import fetch from 'node-fetch';
+// By WillZek
+
 const handler = async (m, {conn, text, usedPrefix, command}) => {
 
-if (!text) throw `*🧑‍💻 Ingresa un texto para generar tu imagen a tu gusto*`;
+if (!text) return m.reply(`*🧑‍💻 Ingresa un texto para generar tu imagen a tu gusto*`);
+
 m.react('🕒');
 await conn.sendMessage(m.chat, {text: '*🧑‍💻 Espere, Estamos Trabajando en su imagen*'}, {quoted: m});
 try {
-const response = await fetch(`https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${encodeURIComponent(text)}`);
+const crow = `https://eliasar-yt-api.vercel.app/api/ai/text2img?prompt=${text}`;
 
-if (!response.ok) throw new Error('Network response was not ok');
-const buffer = await response.buffer();
-m.react('✔️');
-await conn.sendMessage(m.chat, {image: buffer}, {quoted: m});
+const texto = `*Resultados De:* ${text}`;
+m.react(rwait);
+await conn.sendMessage(m.chat, { image: { url: crow }, caption: texto }, { quoted: m });
+m.react(done);
+
 } catch (error) {
 console.error(error);
 throw `*🚨 Lo sentimos, ha ocurrido un error 😔*`;
 }
 }
 handler.tags = ['tools'];
-handler.help = ['genimg'];
-handler.command = ['genearimg2', 'imgg2', 'meta', 'genimg'];
+handler.help = ['crowai'];
+handler.command = ['crowai'];
 
 export default handler;
