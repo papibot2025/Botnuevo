e79c8932ef6361a7ed037c49a206aac6a1abe2b7
Emanuel1273 <willzekoficial@gmail.com>
2025-03-06 20:47:53 -0600

    Actualizar buscador-happymod.js

--- plugins/buscador-happymod.js
+++ plugins/buscador-happymod.js
@@ -11,18 +11,23 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 if (!text) return m.reply(`📩 Ingresa Un Texto Para Buscar En Happy Mod\n> Ejemplo: ${usedPrefix + command} Minecraft`);
 
 try {
-let api = `https://dark-core-api.vercel.app/api/search/happymod?key=api&text=${text}`;
+let api = `https://dark-core-api.vercel.app/api/search/happymod?key=dk-vip&text=${text}`;
 
 let response = await fetch(api);
 let json = await response.json();
-let arch = json.results[0];
 
-if (!arch || arch.length === 0) {
+if (!json || json.length === 0) {
     return m.reply(`🍭 No Encontramos Resultados Para : ${text}`);
 }
 
 m.react('🕑');
-let txt = `🍭 *Titulo:* ${arch.name}\n✏️ *Descripción:* ${arch.description}\n🌟 *Estrellas:* ${arch.stars}\n📎 *Link:* ${arch.link}`;
+let txt = `\`🔎 HAPPYMOD SEARCH\`.`
+for (let i = 0; i < (5 <= json.results.length ? 5 : json.results.length); i++) {
+let arch = json.results[i];
+txt += `🍭 *Titulo:* ${arch.name}\n`
+txt += `✏️ *Descripción:* ${arch.description}\n`
+txt += `🌟 *Estrellas:* ${arch.stars}\n`
+txt += `📎 *Link:* ${arch.link}`;
 
 let img = arch.image;
 
@@ -36,5 +41,7 @@ m.react('✖️');
 }
 
 handler.command = ['happymodsearch', 'hpmodsearch', 'hpmsearch'];
+handler.help = ['happymodsearch'];
+handler.tag = ['buscador'];
 
 export default handler;
