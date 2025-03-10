c7219c84e258244ded324af81ad6a2f6dd5217d7
Emanuel1273 <willzekoficial@gmail.com>
2025-03-09 18:45:52 -0600

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -4,7 +4,7 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 
 let grupos = `╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈≫\n\n☕︎︎ *Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad oficial* 💛
 
-1- 『✯ ⏤͟͞ू⃪𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 *SonicBot*࿐ ✯』
+1- 『✯ ⏤͟͞ू⃪𝗧𝗲𝗮𝗺 𝗖𝗼𝗺𝘂𝗻𝗶𝘁𝘆 SonicBot࿐ ✯』
 *❑* ${grupo}
 
 ✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧⋄⋆⋅⋆⋄✧
