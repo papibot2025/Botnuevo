4303718acb8d19d851cdf00e3b5c3fb0b4f24f2a
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-03-29 11:51:40 -0600

    Actualizar ff-scrim.js

--- plugins/ff-scrim.js
+++ plugins/ff-scrim.js
@@ -1,8 +1,5 @@
-import fg from 'api-dylux' 
-import fetch from 'node-fetch'
-import axios from 'axios'
 let handler = async (m, { conn, args, command, usedPrefix }) => {
-if (!args[0]) throw `
+if (!args[0]) return m.reply(`
 • 𝐒𝐂𝐑𝐈𝐌 •
 
 ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎                       •
@@ -23,7 +20,7 @@ if (!args[0]) throw `
     🥷🏻 ┇ 
     🥷🏻 ┇
                  
-` 
+`) 
 }
 handler.help = ['scrim']
 handler.tags = ['freefire']
