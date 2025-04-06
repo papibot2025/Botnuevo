74ff769124582d3820d726331ec92a7d6e1dbd20
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-05 19:22:31 -0600

    Actualizar buscador-yahoo.js

--- plugins/buscador-yahoo.js
+++ plugins/buscador-yahoo.js
@@ -16,6 +16,7 @@ if (!args[0]) return conn.reply(m.chat, '🌙 INGRESE UN TEXTO PARA BUSCAR EN YA
 try {
 let api = await (await fetch(`https://archive-ui.tanakadomp.biz.id/search/yahoosearch?q=${args[0]}`)).json();
 
+m.react('🕒');
 let moon = `\`𝚈𝙰𝙷𝙾𝙾 𝑋 𝙱𝚄𝚂𝚀𝚄𝙴𝙳𝙰\`.`
 for (let i = 0; i < (5 <= api.result.length ? 5 : api.result.length); i++) {
 
@@ -29,6 +30,7 @@ moon += `───── ･ ｡ﾟ☆: *.☽ .* :☆ﾟ. ─────`
 }
 
 conn.sendMessage(m.chat, { text: moon }, { quoted: m });
+m.react('✅');
 
 } catch (e) {
 m.reply(`*Error En La Api*`);
