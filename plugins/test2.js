ee9ab68440fe1057cdd8c32b18c804461d3141ea
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-06 21:15:47 -0600

    Crear test2.js

---
+++ plugins/test2.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch';
+
+let MF = async(m, { conn, usedPrefix, command, args }) => {
+
+if (!args[0]) return m.reply(`🌃 Por Favor Ingrese El Link Junto Al Comando\n> *Ejemplo:* ${usedPrefix + command} https://www.facebook.com/share/r/12BFZAtjpS8/?mibextid=qDwCgo`);
+
+let fbDL = await facebookdl(args[0]);
+
+let vid = fbDL.pene;
+
+conn.sendMessage(m.chat, { video: { url: vid }, mimetype: 'video/mp4' }, { quoted: m });
+}
+
+MF.command = ['fbdl', 'facebookdl'];
+
+export default MF;
+
+async function facebookdl(url);
+let moon = await(await fetch(`https://vapis.my.id/api/fbdl?url=${url}`)).json();
+return moon;
