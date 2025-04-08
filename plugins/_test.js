2577c1b5ce90a144a45fa3fbb9d57df18731c554
⚡︎🜲𝖂іᥣᥣzׁׅ֬ᥱƙׁׅ-᥆𝖿іᥴіᥲᥣ࿐ <ninopina10@gmail.com>
2025-04-08 15:58:55 -0600

    Actualizar _test.js

--- plugins/_test.js
+++ plugins/_test.js
@@ -9,7 +9,9 @@ if (!args[0]) return m.reply('🌃 Ingrese Para Buscar En Google');
 let moon = args[0];
 let force = `https://api.siputzx.my.id/api/tools/ssweb?url=https://www.google.com/search?q=${moon}`;
 
-conn.sendMessage(m.chat, { image: { url: force }, caption: `*Resultados De: ${moon}` }, { quoted: m });
+let txt = `*Resultados De: ${args[0]}`;
+
+conn.sendMessage(m.chat, { image: { url: force }, caption: txt }, { quoted: m });
 }
 
 MF.command = ['google'];
